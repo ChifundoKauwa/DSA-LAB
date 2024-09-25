efc3100f878d6264e35391d3f024710cc158e515
ChifundoKauwa <bsc-com-15-21@unima.ac.mw>
2024-09-25 15:30:28 +0200

    c++ function template

---
+++ FunctionTemplate.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+using namespace std; 
+ 
+template <class T> 
+T GetMax (T a, T b) { 
+  T result; 
+  result = (a>b)? a : b; 
+  return (result); 
+} 
+ 
+int main () { 
+  int i = 5;  
+  int j = 6,  
+  int k; 
+  long l = 10; 
+  long m = 5,  
+  long n; 
+ 
+  k = GetMax<int>(i,j); 
+ 
+  n = GetMax<long>(l,m); 
+ 
+  cout << k << endl; 
+  cout << n << endl; 
+   
+  return 0;
+}
